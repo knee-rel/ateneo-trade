80682e8b0e9131f279e467778eed2e5c920bfc52
Nirel Ibarra <62606372+knee-rel@users.noreply.github.com>
2024-02-09 17:37:05 +0800

    fetching products from backend

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import Product from "../components/Product";
-import products from "../products";
 
 const HomeScreen = () => {
+  const [products, setProducts] = useState();
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get(
+        "https://api-atrade.onrender.com/api/products"
+      );
+      setProducts(data);
+    };
+
+    fetchProducts();
+  });
+
   return (
     <div className="flex-col w-full justify-center p-10">
       <h1 className="text-amber-500 font-bold text-3xl">
