5f3bc3d8bf811c0ee0612e9c77827f9b5d1f49e3
Nirel Ibarra <62606372+knee-rel@users.noreply.github.com>
2024-02-10 00:04:48 +0800

    readded the error portion for screens

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -12,6 +12,8 @@ const HomeScreen = () => {
         <div className="flex items-center justify-center h-screen">
           <LoadingSpinner />
         </div>
+      ) : error ? (
+        <div>{error?.data.message || error.error}</div>
       ) : (
         <div className="flex-col w-full justify-center p-10">
           <h1 className="text-amber-500 font-bold text-3xl">
